2f3e23273603fbad4cbf37d617ec71971cb1dba1
chao <1552138419@qq.com>
2016-10-26 15:39:39 +0800

    s

--- survey.php
+++ survey.php
@@ -4,12 +4,12 @@
 
 	
 	//header("Content-Type: text/html;charset=utf-8"); 
-	error_reporting(E_ALL ^ E_DEPRECATED);
-	// echo "hehe";exit;
-	$link=mysql_connect('121.42.136.52','one','redhatredhat') or die("无法连接到数据库");//连接到数据库
-	$db_exist=mysql_select_db('ca');
-	if(!$db_exist)
-		echo "hehe";
+	// error_reporting(E_ALL ^ E_DEPRECATED);
+	// // echo "hehe";exit;
+	// $link=mysql_connect('121.42.136.52','one','redhatredhat') or die("无法连接到数据库");//连接到数据库
+	// $db_exist=mysql_select_db('ca');
+	// if(!$db_exist)
+	// 	echo "hehe";
 	// echo $db_exist;exit;
 	// $select="select * from ca.ca";
 	// $result=mysql_query($select);
@@ -33,6 +33,16 @@
 	// 	 )";
 	// 	mysql_query($create_t);
 	// }
+	$servername="121.42.136.52";
+	$username="one";
+	$password="redhatredhat";
+	$dbname="ca";
+	// 创建连接
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// 检测连接
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 	//获取post的数据
 	$name = $_POST['name'];
 	$email = $_POST['email'];
@@ -76,28 +86,28 @@
 		echo json_encode(array('status'=>'error','msg'=>'please fill the form completely'));
 		exit;
 	}
-	//检查手机号、邮箱是否存在
-	$has_phone = mysql_query("select * from ca where phone='$phone'");
-	if(mysql_num_rows($has_phone)){
-		echo json_encode(array('status'=>'error','msg'=>'the phone number is registered'));
-		exit;
-	}
-	$has_email = mysql_query("select * from ca where email='$email'");
-	if(mysql_num_rows($has_email)){
-		echo json_encode(array('status'=>'error','msg'=>'the emailbox is registered'));
-		exit;
-	}
+	// //检查手机号、邮箱是否存在
+	// $has_phone = mysql_query("select * from ca where phone='$phone'");
+	// if(mysql_num_rows($has_phone)){
+	// 	echo json_encode(array('status'=>'error','msg'=>'the phone number is registered'));
+	// 	exit;
+	// }
+	// $has_email = mysql_query("select * from ca where email='$email'");
+	// if(mysql_num_rows($has_email)){
+	// 	echo json_encode(array('status'=>'error','msg'=>'the emailbox is registered'));
+	// 	exit;
+	// }
 	//存入数据库
 	// mysql_query('set names utf8;'); 
 
-	$insert = "insert into ca.ca(id,name,company,job,phone,email,question1,question2,question3_1,question3_2,question3_3,question3_4,question3_5,question3_6,question3_7,question4,question5,question6,question7) values(NULL,'$name','$company','$job','$phone','$email','$question1','$question2','$question3_1','$question3_2','$question3_3','$question3_4','$question3_5','$question3_6','$question3_7','$question4','$question5','$question6','$question7')";
-echo $insert;
-	mysql_query($insert);
+	$insert = "insert into ca(id,name,company,job,phone,email,question1,question2,question3_1,question3_2,question3_3,question3_4,question3_5,question3_6,question3_7,question4,question5,question6,question7) values(NULL,'$name','$company','$job','$phone','$email','$question1','$question2','$question3_1','$question3_2','$question3_3','$question3_4','$question3_5','$question3_6','$question3_7','$question4','$question5','$question6','$question7')";
 
-	if(mysql_affected_rows()<=0){
-		echo json_encode(array('status'=>'error','msg'=>'add data errorly'));
-		exit;
-	}
-	mysql_close($link);
+	mysqli_query($conn,$insert);
+
+	// if(mysql_affected_rows()<=0){
+	// 	echo json_encode(array('status'=>'error','msg'=>'add data errorly'));
+	// 	exit;
+	// }
+	// mysql_close($link);
 	echo json_encode(array("status"=>"success","msg"=>"add data successfully"));
 ?>
