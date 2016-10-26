7e80d1a51ce8f7165dee2b3b6fc2cdedc987e6dc
chao <1552138419@qq.com>
2016-10-26 15:07:09 +0800

    s

--- survey.php
+++ survey.php
@@ -86,6 +86,7 @@
 	}
 	//存入数据库
 	mysql_query('set names utf8;'); 
+	echo "hehe";exit;
 	$insert = "insert into ca(id,name,company,job,phone,email,question1,question2,question3_1,question3_2,question3_3,question3_4,question3_5,question3_6,question3_7,question4,question5,question6,question7) values(NULL,$name,$company,$job,$phone,$email,$question1,$question2,$question3_1,$question3_2,$question3_3,$question3_4,$question3_5,$question3_6,$question3_7,$question4,$question5,$question6,$question7)";
 	mysql_query($insert);
 	echo mysql_query($insert);exit;
